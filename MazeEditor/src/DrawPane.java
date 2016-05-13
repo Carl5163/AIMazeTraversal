ffa8320279bfb7a6d69d43e8b6d485df4e7949c7
Carl <silence.is.pointless@gmail.com>
2016-05-13 17:57:52 -0400

    no message

--- MazeEditor/src/DrawPane.java
+++ MazeEditor/src/DrawPane.java
@@ -6,16 +6,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class DrawPane extends JPanel implements ActionListener, MouseListener, MouseMotionListener {
 
-	private static int WALL = 0;
-	private static int ENTRANCE = 1;
-	private static int EXIT = 2;
+	private static final long MAGIC_NUMBER_XOR = 4819776984503323676L;
+	private static final long MAGIC_NUMBER =     5033236748698419776L;
+	private static final int NONE = 0;
+	private static final int WALL = 1;
+	private static final int ENTRANCE = 2;
+	private static final int EXIT = 3;
+	private static final int BORDER = 4;
+	private static final Color[] COLORS = {Color.WHITE, Color.GRAY, Color.GREEN, Color.BLUE, new Color(0,0,0,0)};
 	
 	private int[][] map;
 	private int mapWidth;
@@ -25,6 +38,10 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 	private int mouseX;
 	private int mouseY;
 	private int selectedItem;
+	private JFileChooser fileChooser;
+	private File file;
+	boolean drawSelectedItem;
+
 	
 	
 	public DrawPane(int[][] map, int width, int height) {
@@ -40,6 +57,8 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 		selectedSquareX = -100;
 		selectedSquareY = -100;
 		selectedItem = WALL;
+		fileChooser = new JFileChooser(".");
+		drawSelectedItem = false;
 	}
 	
 	@Override
@@ -56,47 +75,135 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 
 		for(int i = 0; i < mapWidth; i++) {
 			for(int j = 0; j < mapHeight; j++) {	
-				if(map[i][j] == 1) {
-					drawSquare(g, Color.GRAY, Color.BLACK, i, j);
-				} else {
-					drawSquare(g, Color.WHITE, Color.BLACK, i, j);					
-				}
+				drawSquare(g, COLORS[map[i][j]], Color.BLACK, i*32, j*32);
 			}
 		}
 
 
 		if(selectedSquareX >= 0 && selectedSquareX < mapWidth && selectedSquareY >= 0 && selectedSquareY < mapHeight) {
-			if(map[selectedSquareX][selectedSquareY] == 1) {
-				drawSquare(g, new Color(0,0,0,0), Color.YELLOW, selectedSquareX, selectedSquareY);
-			} else {
-				drawSquare(g, new Color(0,0,0,0), Color.RED, selectedSquareX, selectedSquareY);
-			}
+			drawSquare(g, COLORS[BORDER], Color.RED, selectedSquareX*32, selectedSquareY*32);
 		}
 		
-		if(selectedItem == WALL) {
-			drawSquare(g, Color.GRAY, Color.BLACK, mouseX, mouseY);
-		} else if(selectedItem == ENTRANCE) {
-			drawSquare(g, Color.GREEN, Color.BLACK, mouseX, mouseY);
-		} else if(selectedItem == WALL) {
-			drawSquare(g, Color.BLUE, Color.BLACK, mouseX, mouseY);
+		if(drawSelectedItem) {
+			drawSquare(g, COLORS[selectedItem], Color.BLACK, mouseX-16, mouseY-16);
 		}
-		
 		
 		g.setColor(oldColor);
 		
 		
 	}
 
-	private void drawSquare(Graphics2D g, Color fillColor, Color borderColor, int x, int y) {
-		g.setColor(fillColor);
-		g.fillRect((x*32), (y*32), 32, 32);
-		g.setColor(borderColor);
-		g.drawRect((x*32), (y*32), 32, 32);
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String cmd;
+		cmd = e.getActionCommand();
+		if(cmd.equals("CLEAR")) {
+			if(JOptionPane.showConfirmDialog(this, "Are you sure you want to erase the map?", "Clear Map", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+				initMap();
+			}
+		} else if(cmd.equals("NEW")) {
+			file = null;
+			initMap();
+		} else if(cmd.equals("SELECTED_ITEM")) {
+			selectedItem = ((JComboBox)e.getSource()).getSelectedIndex()+1;
+		} else if(cmd.equals("SAVE")) {
+			File potentialNewFile;
+			if(file != null) { 
+				saveMap(file);
+			} else {
+				potentialNewFile = getSaveFilename();
+				if(potentialNewFile != null) {
+					saveMap(potentialNewFile);
+				}
+			}
+		} else if(cmd.equals("SAVE_AS")) {
+			file = getSaveFilename();
+			if(file != null) {
+				saveMap(file);
+			}
+		} else if(cmd.equals("OPEN")) {
+			File potentialNewFile;
+			if(fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+				potentialNewFile = fileChooser.getSelectedFile();
+				if(!potentialNewFile.exists()) {
+					JOptionPane.showMessageDialog(this, "The file\"" + potentialNewFile + "\" could not be found.", "Open File", JOptionPane.ERROR_MESSAGE);
+				} else {
+					openMap(potentialNewFile);
+				}
+			}
+		}
+	}
+	
+	private File getSaveFilename() {
+		File file;
+		int result;
+		file = null;
+		result = fileChooser.showSaveDialog(this);
+		while(result != JFileChooser.CANCEL_OPTION && file == null) {
+			file = fileChooser.getSelectedFile();
+			if(file.exists()) {
+				if((JOptionPane.showConfirmDialog(this, "The file \"" + file + "\" already exists. Do you want to overwrite it?", "Save File", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.NO_OPTION)) {
+					file = null;
+				}
+			}
+			if(file == null) {
+				result = fileChooser.showSaveDialog(this);
+			}
+		}
+		return file;
+	}
+		
+	private void saveMap(File f) {
+		long encryptedMagicNumber;
+		try {
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(f));
+			encryptedMagicNumber = MAGIC_NUMBER ^ MAGIC_NUMBER_XOR;
+			dos.writeLong(encryptedMagicNumber);
+			for(int i = 0; i < mapWidth; i++) {
+				for(int j = 0; j < mapHeight; j++) {
+					dos.writeByte(map[i][j]);
+				}
+			}
+			file = f;
+			dos.close();
+			
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "Unexpected error while attempting to save file.", "Save File", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+	}
+	
+	private void openMap(File f) {
+		long encryptedMagicNumber;
+		long magicNumber;
+		try {
+			DataInputStream dis = new DataInputStream(new FileInputStream(f));
+			encryptedMagicNumber = dis.readLong();
+			magicNumber = encryptedMagicNumber ^ MAGIC_NUMBER_XOR;
+			if(magicNumber == MAGIC_NUMBER) {
+				for(int i = 0; i < mapWidth; i++) {
+					for(int j = 0; j < mapHeight; j++) {
+						map[i][j] = dis.readByte();
+					}
+				}
+				file = f;
+				repaint();
+				dis.close();
+			
+			} else {
+				JOptionPane.showMessageDialog(this, "The file you chose is not a valid map file.", "Open File", JOptionPane.ERROR_MESSAGE);
+			}
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "Unexpected error while attempting to save file.", "Save File", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		
+	private void drawSquare(Graphics2D g, Color fillColor, Color borderColor, int x, int y) {
+		g.setColor(fillColor);
+		g.fillRect(x, y, 32, 32);
+		g.setColor(borderColor);
+		g.drawRect(x, y, 32, 32);
 	}
 
 	@Override
@@ -105,30 +212,12 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 		selectedSquareY = e.getY()/32;
 		if(selectedSquareX >= 0 && selectedSquareX < mapWidth && selectedSquareY >= 0 && selectedSquareY < mapHeight) {
 			if(e.getButton() == MouseEvent.BUTTON1) {
-				map[selectedSquareX][selectedSquareY] = 1;
-			} else if(e.getButton() == MouseEvent.BUTTON2){
+				map[selectedSquareX][selectedSquareY] = selectedItem;
+			} else if(e.getButton() == MouseEvent.BUTTON3){
 				map[selectedSquareX][selectedSquareY] = 0;
 			}
 			repaint();
 		}
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-
-		
 	}
 
 	@Override
@@ -139,7 +228,7 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 		selectedSquareY = mouseY/32;
 		if(selectedSquareX >= 0 && selectedSquareX < mapWidth && selectedSquareY >= 0 && selectedSquareY < mapHeight) {
 			if(SwingUtilities.isLeftMouseButton(e)) {
-				map[selectedSquareX][selectedSquareY] = 1;
+				map[selectedSquareX][selectedSquareY] = selectedItem;
 			} else if(SwingUtilities.isRightMouseButton(e)){
 				map[selectedSquareX][selectedSquareY] = 0;
 			}
@@ -158,14 +247,15 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		String cmd;
-		cmd = e.getActionCommand();
-		if(cmd.equals("CLEAR")) {
-			if(JOptionPane.showConfirmDialog(this, "Are you sure you want to erase the map?", "Clear Map", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
-				initMap();
-			}
-		}
+	public void mouseEntered(MouseEvent e) {
+		drawSelectedItem = true;
+		repaint();
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {
+		drawSelectedItem = false;
+		repaint();
 	}
 	
 	private void initMap() {
@@ -175,6 +265,10 @@ public class DrawPane extends JPanel implements ActionListener, MouseListener, M
 			}
 		}
 	}
-	
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	@Override
+	public void mouseClicked(MouseEvent e) {}
 	
 }
--- MazeEditor/src/MazeEditor.java
+++ MazeEditor/src/MazeEditor.java
@@ -6,10 +6,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 
 public class MazeEditor extends JFrame implements ActionListener{
@@ -31,21 +34,37 @@ public class MazeEditor extends JFrame implements ActionListener{
 		mapHeight = height;
 		
 		setSize(1000, 1000);
-		//setResizable(false);
+		setResizable(false);
 		
-		cp = getContentPane();		
-		
+		cp = getContentPane();				
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		drawPane = new DrawPane(map, mapWidth, mapHeight);
 		
 		cp.add(drawPane, BorderLayout.CENTER);
+		cp.add(makeToolPanel(), BorderLayout.EAST);
 		setJMenuBar(makeMenuBar());
 		
 	}
 	
-	
+	private JPanel makeToolPanel() {
+		
+		JPanel panel;
+		JComboBox menu;
+		
+		panel = new JPanel();
+		
+		String[] options = {"Wall", "Entrance", "Exit"};
+		menu = new JComboBox(options);
+		menu.setActionCommand("SELECTED_ITEM");
+		menu.addActionListener(drawPane);
+		
+		panel.add(new JLabel("Object"));
+		panel.add(menu);		
+		
+		return panel;
+	}
 
 	private JMenuBar makeMenuBar() {
 
@@ -62,14 +81,21 @@ public class MazeEditor extends JFrame implements ActionListener{
 		menuItem.setActionCommand("OPEN");
 		menuItem.setMnemonic(KeyEvent.VK_O);
 		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
-		menuItem.addActionListener(this);
+		menuItem.addActionListener(drawPane);
 		menu.add(menuItem);
 		
 		menuItem = new JMenuItem("Save");
 		menuItem.setActionCommand("SAVE");
 		menuItem.setMnemonic(KeyEvent.VK_S);
 		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
-		menuItem.addActionListener(this);
+		menuItem.addActionListener(drawPane);
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Save As");
+		menuItem.setActionCommand("SAVE_AS");
+		menuItem.setMnemonic(KeyEvent.VK_A);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
+		menuItem.addActionListener(drawPane);
 		menu.add(menuItem);
 		
 		menu.addSeparator();
@@ -84,6 +110,7 @@ public class MazeEditor extends JFrame implements ActionListener{
 		menuBar.add(menu);
 		
 		menu = new JMenu("Edit");
+		menu.setMnemonic(KeyEvent.VK_E);
 		
 		menuItem = new JMenuItem("Clear");
 		menuItem.setActionCommand("CLEAR");
@@ -108,8 +135,7 @@ public class MazeEditor extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println(e.getActionCommand());
-		
+		this.dispose();
 	}
 	
 }
